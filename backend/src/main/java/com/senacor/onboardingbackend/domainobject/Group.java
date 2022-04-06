29a70680a08e1ef1771d668cffc55db9b8fc79f2
Sevket Gökay <sevket.goekay@senacor.com>
2022-04-06 13:20:20 +0200

    task 3

---
+++ backend/src/main/java/com/senacor/onboardingbackend/domainobject/Group.java
@@ -0,0 +1,59 @@
+package com.senacor.onboardingbackend.domainobject;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.time.OffsetDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Table(name = "group")
+@Entity
+@Setter
+@Getter
+@ToString
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "date_created")
+    private OffsetDateTime dateCreated;
+
+    @Column(name = "date_meeting")
+    private OffsetDateTime dateMeeting;
+
+    @ManyToMany(mappedBy = "groups")
+    @ToString.Exclude
+    private Set<Person> persons = new HashSet<>();
+
+    /**
+     * Don't change this
+     */
+    @Override
+    public int hashCode() {
+        return 19;
+    }
+
+    /**
+     * Don't change this
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        Group other = (Group) obj;
+        return id != null && id.equals(other.getId());
+    }
+}
---
+++ backend/src/main/java/com/senacor/onboardingbackend/domainobject/Person.java
@@ -0,0 +1,67 @@
+package com.senacor.onboardingbackend.domainobject;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
+
+@Table(name = "person")
+@Entity
+@Setter
+@Getter
+@ToString
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "age")
+    private Integer age;
+
+    @JoinTable(
+        name = "group_person",
+        joinColumns = {@JoinColumn(name = "person_id")},
+        inverseJoinColumns = {@JoinColumn(name = "group_id")})
+    @ManyToMany
+    @ToString.Exclude
+    private Set<Group> groups = new HashSet<>();
+
+    /**
+     * Don't change this
+     */
+    @Override
+    public int hashCode() {
+        return 19;
+    }
+
+    /**
+     * Don't change this
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+
+        Person other = (Person) obj;
+        return id != null && id.equals(other.getId());
+    }
+}
