c106dce3c40623b3f03fad645abf169c2dfdb583
muhlbrock <hendrikmuehlenbrock@gmx.de>
2022-06-08 17:04:04 +0200

    Added: CRUD Operations

--- code-with-quarkus/src/main/java/org/acme/controllers/PersonController.java
+++ code-with-quarkus/src/main/java/org/acme/controllers/PersonController.java
@@ -24,7 +24,7 @@ public class PersonController {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getById(@PathParam("id") Long personId){
         return Person.findByIdOptional(personId)
-                .map(person -> Response.ok().build())
+                .map(person -> Response.ok(person).build())
                 .orElse(Response.status(Response.Status.NOT_FOUND).build());
     }
 
@@ -35,8 +35,47 @@ public class PersonController {
     public Response create(Person person){
         Person.persist(person);
         if(person.isPersistent()){
-            return Response.created(URI.create("/persons" + person.getId())).build();
+            return Response.created(URI.create("/persons" + person.id)).build();
         }
         return Response.status(Response.Status.BAD_REQUEST).build();
     }
+
+    @PUT
+    @Transactional
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response put(@PathParam("id") Long id, Person person){
+        if (person.firstName.equals("") || person.lastName.equals("") || person.age < 0){
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        Person personToUpdate = Person.findById(id);
+
+        if(personToUpdate == null){
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        personToUpdate.firstName = person.firstName;
+        personToUpdate.lastName = person.lastName;
+        personToUpdate.age = person.age;
+
+        personToUpdate.persist();
+        if(personToUpdate.isPersistent()){
+            return Response.created(URI.create("/persons" + person.id)).build();
+        }
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
+    @DELETE
+    @Transactional
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteById(@PathParam("id") Long id){
+        boolean deleted = Person.deleteById(id);
+        if (deleted){
+            return Response.noContent().build();
+        } else {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
 }
