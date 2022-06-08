f0a2beb0d2d2946817aff6696556e348ca629d27
muhlbrock <hendrikmuehlenbrock@gmx.de>
2022-06-08 10:56:00 +0200

    Added: Person Entity

---
+++ code-with-quarkus/src/main/java/org/acme/data/entities/Person.java
@@ -0,0 +1,16 @@
+package org.acme.data.entities;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+import javax.persistence.Column;
+
+public class Person extends PanacheEntity {
+
+    @Column
+    public String firstName;
+    @Column
+    public String lastName;
+    @Column
+    public int age;
+
+}
