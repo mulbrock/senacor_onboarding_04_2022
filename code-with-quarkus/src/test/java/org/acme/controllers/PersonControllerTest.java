5a64afd04bc585ffcd49226e3a2bdfef1d69767d
muhlbrock <hendrikmuehlenbrock@gmx.de>
2022-06-10 11:14:44 +0200

    Added: PersonControllerTest

---
+++ code-with-quarkus/src/test/java/org/acme/controllers/PersonControllerTest.java
@@ -0,0 +1,21 @@
+package org.acme.controllers;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class PersonControllerTest {
+
+    @Test
+    public void testPersonsEndpoint(){
+        given()
+                .when().get("/persons")
+                .then()
+                .statusCode(200)
+                .body(is("[]"));
+    }
+
+}
