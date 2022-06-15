60fbac9ea55a1b8c0a9cec818a3761d3da1dad82
muhlbrock <hendrikmuehlenbrock@gmx.de>
2022-06-15 09:55:55 +0200

    Added: ListComponent

--- frontend/frontend/src/app/components/listings/list/list.component.ts
+++ frontend/frontend/src/app/components/listings/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {PersonInterface} from "../../../interfaces/person-interface";
+import {Observable} from "rxjs";
+import {GlobalStateService} from "../../../services/global-state.service";
 
 @Component({
   selector: 'app-list',
@@ -7,9 +10,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  public personList!: Observable<Array<PersonInterface>>;
+
+  constructor(private globalStateService: GlobalStateService) { }
 
   ngOnInit(): void {
+    this.personList = this.globalStateService.currentData;
   }
 
 }
