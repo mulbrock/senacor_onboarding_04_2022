4c330ae87fbfbeb8f4edd38ade9a55e1ffa85922
muhlbrock <hendrikmuehlenbrock@gmx.de>
2022-06-15 08:38:52 +0200

    Updated: app module

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule} from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { ListComponent } from './components/listings/list/list.component';
+import { ListItemComponent } from './components/listings/listitem/list-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
